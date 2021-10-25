41cb7ca40ba3b46246e4a1698e4595d555cdf6cc
durgeshmeena <durgeshkumar@iitg.ac.in>
2021-10-26 00:32:06 +0530

    adding session for a user

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
+from datetime import time
 import re
 from typing import Tuple
 from flask import Flask, request, session
 from twilio.twiml.messaging_response import Body, Message, Redirect, MessagingResponse
-import json
 from decouple import config
-
+import time
 import pandas as pd 
 
 from update_mlh_data import mlh_update
@@ -12,6 +12,7 @@ from update_cp_data import cp_update
 from update_hackerearth_data import hackerearth_update
 app = Flask(__name__)
 app.secret_key = config('APP_SECRET')
+    # session = {'login':False, 'loaded':True, 'catagory':None, 'stage':0, 'CP':{ 'catagory':False, 'stage':0 }, 'MLH':{ 'catagory':False, 'stage':0 }, 'Hack':{ 'catagory':False, 'stage':0 }, }
 
 
 @app.route('/')
@@ -20,21 +21,37 @@ def index():
 
 @app.route( '/bot', methods=['POST']) 
 def bot():
-    session["Start"] = True
-    # session = {'Login':False, 'Loaded':True, 'Catagory':None, 'Stage':0, 'CP':{ 'Catagory':False, 'Stage':0 }, 'MLH':{ 'Catagory':False, 'Stage':0 }, 'Hack':{ 'Catagory':False, 'Stage':0 }, }
+    # print(session['login'])
     incoming_msg = request.values.get('Body', '').lower()
     response = MessagingResponse()
+    # session = {'login':False, 'loaded':True, 'catagory':None, 'stage':0, 'CP':{ 'catagory':False, 'stage':0 }, 'MLH':{ 'catagory':False, 'stage':0 }, 'Hack':{ 'catagory':False, 'stage':0 }, }
+    if "show" in incoming_msg:
+        session['login'] = True
+        session['loaded'] = True
+        session['catagory'] = None
+        session['cp'] = 0
+        session['mlh'] = 0
+        session['hack'] = 0
+        response.message('Choose one of the following to get imformation\n*1.* *Hackathon*- for Hackathons \n*2* *CP*- Contest \n[ _after_ _loading_ _eventdata_, \n_type_ *_Next_* _to_ _load_ _more_ _data_ ] \n\n*?*- for other commands')
+        return str(response)
+    else:
+        if 'login' in session:
+            pass
+        else:
+            response.message('*Welcome user* \nwe are one step solution for all hackathons and contest \ntype *Show* to show available options')
+            return str(response)               
+        
 
-    if session['Login']:
+    if session['login']:
         if '?' in incoming_msg:
-            if session['Loaded']:
+            if session['loaded']:
                 response.message('\n*1.* *Hackathon*- for Hackathons \n*2.* *CP*- Contest \n *3.* *Home*- for home \n *4.* *Logout*- for ending session \n *5.* *UpdateM*- for refreshing MLH events data \n *6.* *UpdateC*- for refreshing Contest events data \n *7.* *UpdateO*- for refreshing Other Hackathons events data \n *8.* *Next*- to load more next data')
                 responded = True  
             else:
                 response.message('still Loading!. Please wait for few seconds')
                 responded = True 
 
-        if not session['Loaded']: 
+        if not session['loaded']: 
 
             if 'mlh' or 'cp' or 'hacks' or 'updatemlh' or 'updatecp' or 'updatehacks' in incoming_msg:
                 response.message("*Loading data*\nthis may take some time. Please reply with *?* to know status of your information")
@@ -42,9 +59,7 @@ def bot():
     
 
             elif 'logout' in incoming_msg:
-                session['Login'] = False
-                session['Catagory'] = None
-                session['CP']['Stage'] = session['Hack']['Stage'] = session['MLH']['Stage'] = 0 
+                session.clear()
                 response.message('session ended sucessfully! \n*Thank you*')
                 responded = True
             else:
@@ -52,74 +67,77 @@ def bot():
                 responded = True
                 
    
-        if session['Loaded']:
+        if session['loaded']:
             if 'hackathon' in incoming_msg:
                 response.message('Choose one of the following to get imformation\n*1.* *MLH*- for Mlh Hackathons \n*2.* *Other*- for Other Hackathons')
                 responded = True
 
             elif 'other' in incoming_msg:
-                session['Catagory'] = 'Hack'
+                session['catagory'] = 'hack'
                 df = pd.read_csv('data_hackerearth.csv')
-                i = session['Hack']['Stage'] = 0
-                session['Hack']['Stage'] = session['Hack']['Stage']+5
-                # print('i=',i, ' --- ', 'new stage= ', session['Stage'])
-                while i<session['Hack']['Stage']:
+                i = 0
+                session['hack'] = session['hack']+5
+                # print('i=',i, ' --- ', 'new stage= ', session)
+                while i<=session['hack']:
                     msg = response.message('Category: '+ '*'+ df.iloc[i, :]['Category'] +'*'+'\n' +'Name: ' + '*'+df.iloc[i, :]['Name']  +'*'+'\n' +      'Status: ' +'*'+df.iloc[i, :]['Status']   +'*'+'\n' + 'Link: '+   '*'+df.iloc[i, :]['Link']      +'*'+'\n' +  'Type: ' + '*'+df.iloc[i, :]['Type']  + '*'+'\n' + 'Company: ' + '*'+df.iloc[i, :]['Company']  + '*'+'\n'+ 'Registrations/Prize: ' + '*'+  str( df.iloc[i, :]['Registrations/Prize'] ) + '*'+'\n'  )
                     msg.media(df.iloc[i,:]['image'])
+                    if i==session['hack']:
+                        response.message('type *Next* to load more...')
                     i+=1
-                response.message('type *Next* to load more...') 
                 responded = True     
 
             elif 'mlh' in incoming_msg:
-                session['Catagory'] = 'MLH'
+                session['catagory'] = 'mlh'
                 df = pd.read_csv('data_mlh.csv')
-                i = session['MLH']['Stage'] = 0
-                session['MLH']['Stage'] = session['MLH']['Stage']+5
-                # print('i=',i, ' --- ', 'new stage= ', session['Stage'])
-                while i<session['MLH']['Stage']:
+                i = session['mlh'] = 0
+                session['mlh'] = session['mlh']+5
+                # print('i=',i, ' --- ', 'new stage= ', session)
+                while i<=session['mlh']:
                     msg = response.message('Category: '+ '*'+ df.iloc[i, :]['Category'] +'*'+'\n' +'Name: ' + '*'+df.iloc[i, :]['Name']  +'*'+'\n' +      'Date/Time: ' +'*'+df.iloc[i, :]['Date/Time']   +'*'+'\n' + 'Length: ' +'*'+df.iloc[i, :]['Length']     +'*'+'\n' +  'Link: '+   '*'+df.iloc[i, :]['Link']      +'*'+'\n' +  'Location: ' + '*'+df.iloc[i, :]['Location'] +'*'+'\n' +       'Type: ' + '*'+df.iloc[i, :]['Type']  + '*'+'\n'  )
                     msg.media(df.iloc[i,:]['Image'])
+                    if i==session['mlh']:
+                        response.message('type *Next* to load more...')
                     i+=1
-                response.message('type *Next* to load more...') 
-                responded = True                         
+                responded = True                        
               
 
             elif 'cp' in incoming_msg:
-                session['Catagory'] = 'CP'
+                session['catagory'] = 'cp'
                 df = pd.read_csv('data_cp.csv')
-                i = session['CP']['Stage'] = 0
-                session['CP']['Stage'] = session['CP']['Stage']+5
-                # print('i=',i, ' --- ', 'new stage= ', session['Stage'])
-                while i<session['CP']['Stage']:
+                i = session['cp'] = 0
+                session['cp'] = session['cp']+5
+                # print('i=',i, ' --- ', 'new stage= ', session)
+                while i<=session['cp']:
                     response.message('Category: '+ '*'+ df.iloc[i, :]['Category'] +'*'+'\n' +'Name: ' + '*'+df.iloc[i, :]['Name']  +'*'+'\n' +      'Date/Time: ' +'*'+df.iloc[i, :]['Date/Time']   +'*'+'\n' + 'Length: ' +'*'+df.iloc[i, :]['Length']     +'*'+'\n' +  'Link: '+   '*'+df.iloc[i, :]['Link']      +'*'+'\n' +  'Location: ' + '*'+df.iloc[i, :]['Location'] +'*'+'\n' +       'Type: ' + '*'+df.iloc[i, :]['Type']  + '*'+'\n'  )
+                    if i==session['cp']:
+                        response.message('type *Next* to load more...')
                     i+=1
-                response.message('type *Next* to load more...') 
                 responded = True
 
             elif 'next' in incoming_msg:
                        
-                if session['Catagory'] == 'CP':
+                if session['catagory'] == 'cp':
                     df = pd.read_csv('data_cp.csv')
-                    i = session['CP']['Stage']
-                    session['CP']['Stage'] = session['CP']['Stage']+5
-                    while i<session['CP']['Stage']:
+                    i = session['cp']
+                    session['cp'] = session['cp']+5
+                    while i<session['cp']:
                         response.message('Category: '+ '*'+ df.iloc[i, :]['Category'] +'*'+'\n' +'Name: ' + '*'+df.iloc[i, :]['Name']  +'*'+'\n' +      'Date/Time: ' +'*'+df.iloc[i, :]['Date/Time']   +'*'+'\n' + 'Length: ' +'*'+df.iloc[i, :]['Length']     +'*'+'\n' +  'Link: '+   '*'+df.iloc[i, :]['Link']      +'*'+'\n' +  'Location: ' + '*'+df.iloc[i, :]['Location'] +'*'+'\n' +       'Type: ' + '*'+df.iloc[i, :]['Type']  + '*'+'\n'  )
                         i+=1
 
-                elif session['Catagory'] == 'Hack':
+                elif session['catagory'] == 'hack':
                     df = pd.read_csv('data_hackerearth.csv')
-                    i = session['Hack']['Stage'] 
-                    session['Hack']['Stage'] = session['Hack']['Stage']+5
-                    while i<session['Hack']['Stage']:
+                    i = session['hack'] 
+                    session['hack'] = session['hack']+5
+                    while i<session['hack']:
                         msg = response.message('Category: '+ '*'+ df.iloc[i, :]['Category'] +'*'+'\n' +'Name: ' + '*'+df.iloc[i, :]['Name']  +'*'+'\n' +      'Status: ' +'*'+df.iloc[i, :]['Status']   +'*'+'\n' + 'Link: '+   '*'+df.iloc[i, :]['Link']      +'*'+'\n' +  'Type: ' + '*'+df.iloc[i, :]['Type']  + '*'+'\n' + 'Company: ' + '*'+df.iloc[i, :]['Company']  + '*'+'\n'+ 'Registrations/Prize: ' + '*'+  str( df.iloc[i, :]['Registrations/Prize'] ) + '*'+'\n'  )
                         msg.media(df.iloc[i,:]['image'])
                         i+=1
 
-                elif session['Catagory'] == 'MLH':
+                elif session['catagory'] == 'mlh':
                     df = pd.read_csv('data_mlh.csv')
-                    i = session['MLH']['Stage']
-                    session['MLH']['Stage'] = session['MLH']['Stage']+5
-                    while i<session['MLH']['Stage']:
+                    i = session['mlh']
+                    session['mlh'] = session['mlh']+5
+                    while i<session['MLH']:
                         msg = response.message('Category: '+ '*'+ df.iloc[i, :]['Category'] +'*'+'\n' +'Name: ' + '*'+df.iloc[i, :]['Name']  +'*'+'\n' +      'Date/Time: ' +'*'+df.iloc[i, :]['Date/Time']   +'*'+'\n' + 'Length: ' +'*'+df.iloc[i, :]['Length']     +'*'+'\n' +  'Link: '+   '*'+df.iloc[i, :]['Link']      +'*'+'\n' +  'Location: ' + '*'+df.iloc[i, :]['Location'] +'*'+'\n' +       'Type: ' + '*'+df.iloc[i, :]['Type']  + '*'+'\n'  )
                         msg.media(df.iloc[i,:]['Image'])
                         i+=1
@@ -146,9 +164,7 @@ def bot():
                 responded = True
 
             elif 'logout' in incoming_msg:
-                session['Login'] = False
-                session['Catagory'] = None
-                session['CP']['Stage'] = session['Hack']['Stage'] = session['MLH']['Stage'] = 0 
+                session.clear()
                 response.message('session ended sucessfully! \n*Thank you*')
                 responded = True
 
@@ -157,17 +173,7 @@ def bot():
                 response.message('Type *?* to know your command')     
                 responded = True
             if not responded:
-                response.message('an Error occured! again Type *START* to start bot')  
-         
-    
-    else:
-        if "show" in incoming_msg:
-            session['Login'] = True
-            response.message('Choose one of the following to get imformation\n*1.* *Hackathon*- for Hackathons \n*2* *CP*- Contest \n \n *?*- for other commands')
-            responded = True 
-        else:
-            response.message('*Welcome user* \nwe are one step solution for all hackathons and contest \ntype *Show* to show available options')
-            responded = True             
+                response.message('an Error occured! again Type *START* to start bot')              
 
     return str(response)
 
